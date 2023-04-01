34c0dcefeac2a2fc154fe118d9028dcffff9d4d1
HicaroD <hdanrlley1@gmail.com>
2023-04-01 12:27:48 -0300

    modulo3: updating code after recording

--- modulo3/main.py
+++ modulo3/main.py
@@ -1,4 +1,4 @@
-from fastapi import Depends, FastAPI
+from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 import models
 import schemas
@@ -7,7 +7,7 @@ from database import SessionLocal, engine
 models.Base.metadata.create_all(bind=engine)
 
 
-def get_database():
+def get_database_session():
     database_session = SessionLocal()
     try:
         yield database_session
@@ -18,40 +18,35 @@ def get_database():
 app = FastAPI()
 
 
-@app.get("/estudantes", response_model=list[schemas.BaseEstudante])
-def get_all_students(database: Session = Depends(get_database)):
-    users = database.query(models.Estudante).all()
-    return users
+@app.get("/estudantes", response_model=list[schemas.EstudanteSchema])
+def get_all_students(database: Session = Depends(get_database_session)):
+    students = database.query(models.Estudante).all()
+    return students
 
 
-@app.get("/estudante/{matricula}/", response_model=schemas.BaseEstudante)
-def get_student_by_matricula(matricula: int, database: Session = Depends(get_database)):
-    user = (
+@app.get("/estudante/{matricula}", response_model=schemas.EstudanteSchema)
+def get_student_by_matricula(
+    matricula: int, database: Session = Depends(get_database_session)
+):
+    student = (
         database.query(models.Estudante)
         .filter(models.Estudante.matricula == matricula)
         .first()
     )
-    return user
+    # TODO: lide com o caso onde o estudante não existe
+    return student
 
 
-@app.post("/estudante/create/", response_model=schemas.BaseEstudante, status_code=201)
+@app.post("/estudante/create", response_model=schemas.EstudanteSchema, status_code=201)
 def create_student(
-    estudante: schemas.BaseEstudante,
-    database: Session = Depends(get_database),
+    estudante: schemas.EstudanteSchema,
+    database: Session = Depends(get_database_session),
 ):
-    novo_estudante = models.Estudante(**estudante.dict())
-
-    # O código abaixo é equivalente a linha de cima.
-    # Perceba que os campos são de estudante do model do banco de dados e do
-    # schema da API, então é melhor usar a linha de cima.
-    # Se fossem campos com nomes diferentes, então deveríamos usar o código comentado abaixo
-
-    # novo_estudante = models.Estudante(
-    #     matricula=estudante.matricula,
-    #     nome=estudante.nome,
-    #     idade=estudante.idade,
-    # )
-
+    novo_estudante = models.Estudante(
+        matricula=estudante.matricula,
+        nome=estudante.nome,
+        idade=estudante.idade,
+    )
     database.add(novo_estudante)
     database.commit()
     database.refresh(novo_estudante)
@@ -59,29 +54,32 @@ def create_student(
 
 
 @app.delete("/estudante/delete/{matricula}")
-def delete_student(matricula: int, database: Session = Depends(get_database)):
+def delete_student(matricula: int, database: Session = Depends(get_database_session)):
+    # TODO: cheque se o estudante existe antes de tentar removê-lo
     database.query(models.Estudante).filter(
         models.Estudante.matricula == matricula
     ).delete()
     database.commit()
-    return {
-        "detail": f"Estudante deletado com matricula {matricula} foi removido com sucesso"
-    }
+    return {"detail": "Estudante removido com sucesso"}
 
 
 @app.patch("/estudante/update/{matricula}")
 def update_student(
     matricula: int,
     estudante: schemas.EstudanteUpdate,
-    database: Session = Depends(get_database),
+    database: Session = Depends(get_database_session),
 ):
-    student_from_db = database.query(models.Estudante).filter(
-        models.Estudante.matricula == matricula
-    ).first()
+    student_from_database = (
+        database.query(models.Estudante)
+        .filter(models.Estudante.matricula == matricula)
+        .first()
+    )
+    # TODO: cheque se o estudante existe antes de atualizar
 
     updated_student = estudante.dict(exclude_unset=True)
     for key, value in updated_student.items():
-        setattr(student_from_db, key, value)
+        setattr(student_from_database, key, value)
+
     database.commit()
-    database.refresh(student_from_db)
+    database.refresh(student_from_database)
     return {"detail": "Estudante atualizado com sucesso"}
